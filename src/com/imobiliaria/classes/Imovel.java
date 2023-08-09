988fad7358cc312c289d99ef61b188d585a18fa8
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-09 11:19:55 -0300

    Cria construtor da classe Imovel

--- src/com/imobiliaria/classes/Imovel.java
+++ src/com/imobiliaria/classes/Imovel.java
@@ -8,5 +8,34 @@ public class Imovel {
     private LocalDate dataCadastro;
     private boolean ativo;
 
-    
+    public Imovel(String endereco, LocalDate dataCadastro) {
+        super();
+        this.endereco = endereco;
+        this.dataCadastro = dataCadastro;
+        this.ativo = false;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void ativarCadastro() {
+        this.ativo = true;
+    }
 }
