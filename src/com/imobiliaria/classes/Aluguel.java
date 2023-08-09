fbb3be0aef57490dae39072278463df382db666e
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-09 11:25:03 -0300

    Cria getters e setters da classe Aluguel

--- src/com/imobiliaria/classes/Aluguel.java
+++ src/com/imobiliaria/classes/Aluguel.java
@@ -1,4 +1,30 @@
 package com.imobiliaria.classes;
 
-public class Aluguel {
+import java.time.LocalDate;
+
+public class Aluguel extends Imovel {
+
+    private double valorAluguel;
+    private boolean alugado;
+
+    public Aluguel(String endereco, LocalDate dataCadastro) {
+        super(endereco, dataCadastro);
+        this.alugado = false;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public boolean isAlugado() {
+        return alugado;
+    }
+
+    public void alugar() {
+        this.alugado = true;
+    }
 }
